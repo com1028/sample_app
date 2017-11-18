74f60eb5c92f27ddb7f249d89c88f9d8b5db4682
Yuta komure <com10281028@gmail.com>
2017-11-18 11:38:18 +0900

    Add user micropots

--- config/application.rb
+++ config/application.rb
@@ -17,3 +17,15 @@ module SampleApp
     config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
+module AwesomeEvents
+  class Application < Rails::Application
+    config.autoload_paths += Dir[Rails.root.join('app', 'uploaders')]
+    # 以下省略
+    config.load_defaults 5.1
+
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration should go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
+  end
+end
