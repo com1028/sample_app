029d4ada9081eb9bcc18da2071ee610d6039c4bf
Yuta komure <com10281028@gmail.com>
2017-11-20 01:10:50 +0900

    apurikeawss

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,10 +3,10 @@ if Rails.env.production?
     config.fog_credentials = {
       # Amazon S3用の設定
       :provider              => 'AWS',
-      :region                => ENV['ap-northeast-1'],     # 例: 'ap-northeast-1'
-      :aws_access_key_id     => ENV['AKIAJEC3TSWLAHZ767HQ'],
-      :aws_secret_access_key => ENV['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']
+      :region                => 'ap-northeast-1',     # 例: 'ap-northeast-1'
+      :aws_access_key_id     => 'AKIAJEC3TSWLAHZ767HQ',
+      :aws_secret_access_key => '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'
     }
-    config.fog_directory     =  ENV['com1']
+    config.fog_directory     =  'com1'
   end
 end
