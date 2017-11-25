8336f3b6b0707e33124237909a4c6f1aabd70aca
Yuta komure <com10281028@gmail.com>
2017-11-25 16:46:32 +0900

    1125com

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -34,10 +34,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save # => Validation
-      # Sucess
-      @user.send_activation_email
-      flash[:info] = "Please check your email to activate your account."
-      redirect_to root_url
+      # # Sucess
+      # @user.send_activation_email
+      log_in @user
+      flash[:success] = "Welcome to ようこそ Anime.com！"
+      redirect_to @user
     else
       # Failure
       render 'new'
